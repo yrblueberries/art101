be2600788c1581eca106d53f4551759c88d4f17d
yrblueberries <liamlogsdon2@gmail.com>
2024-10-21 18:15:36 -0700

    lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,28 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author:Liam Logsdon 
+// Date: 21 October
 
 // Constants
-
+make = "Saturn";
+model = "Sl1";
+color = "Blue";
+year = 1999;
 // Functions
-
+age = 2024- year;
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
   // return results;
 }
 
-function main() {
-  console.log("Main function started.");
+
+document.writeln("Make: " + make + "<br>" );
+document.writeln("Model: " + model + "<br>" );
+document.writeln("Color: " + color + "<br>" );
+document.writeln("Year: " + year + "<br>" );
+document.writeln("Age: " + age + " years<br>" );
   // the code that makes everything happen
-}
+
 
 // let's get this party started
 main();
