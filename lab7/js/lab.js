206712a454d4d4a460bea25c39c0aa24374bd730
yrblueberries <liamlogsdon2@gmail.com>
2024-10-28 18:08:46 -0700

    lab first tempt

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,21 +1,23 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Liam Logsdon
+// Date:10/28/24
 
 // Constants
 
 // Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function sortUsername() {
+var userName = window.prompt("whats your name? let me change it")
+console.log("username =", userName);
+// splut string to array
+var nameArray = userName.split('');
+console.log("nameArray =", nameArray);
+//sort array
+var nameArraySort = nameArray.sort();
+console.log("nameArraySort =", nameArraySort);
+//join array back to string
+var nameSorted = nameArraySort.join('');
+console.log("nameSorted =", nameSorted);
+return nameSorted;
 }
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+document.writeln("hey look I changed your name: ",
+sortUsername(), "</br>");
