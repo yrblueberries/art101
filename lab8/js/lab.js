8238eba457ec290fd9d62c86aaa68088655e9db5
yrblueberries <liamlogsdon2@gmail.com>
2024-10-31 20:38:46 -0700

    8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -7,15 +7,26 @@
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function isEven(x) {
+  return (x % 2 == 0);
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+console.log("is 1 even?",isEven(1));
+console.log("is 2 even?",isEven(2));
 
-// let's get this party started
-main();
+array = [120,43,21,46,20,1635,]
+console.log("my Array", array);
+
+var result = array.map(isEven);
+console.log("[percentage of evenness", result);
+
+var result2 = array.map(function(x){
+return x ** 0.5;
+ 
+})
+console.log("square root of an array", result2);
+
+
+document.writeln("[",array,"]", "</br>");
+document.writeln("[",result,"]", "</br>");
+document.writeln("[",result2,"]", "</br>");
