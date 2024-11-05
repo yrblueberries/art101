618f451f86c63162bf1ed16d7e0f212419d7e318
yrblueberries <liamlogsdon2@gmail.com>
2024-11-04 19:07:12 -0800

    BUTTON

--- js/site.js
+++ js/site.js
@@ -1,6 +1,6 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Liam Logsdon
+// Date: 
 
 // Constants
 
--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,21 +1,24 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Liam Logsdon
+// Date:11/4/24
 
 // Constants
 
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+$("#challenge").append("<button id='button-challenge'>secrets</button>").click(function() {
+  // I can use "this" to refer to the button that got clicked
+  // here's a different maybe better way to do it
+  // (we are traversing the DOM to the parent)
+  $(this).parent().toggleClass("special");
+  $(this).parent().toggleClass("minor-sectionc");
+});
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+$(".minor-section").append("<button id='warp'>boring button</button>").click(function() {
+  // I can use "this" to refer to the button that got clicked
+  // here's a different maybe better way to do it
+  // (we are traversing the DOM to the parent)
+  $(this).toggleClass("basic");
+  $(this).toggleClass("minor-section");
+});
